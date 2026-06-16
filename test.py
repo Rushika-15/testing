b17e203953c7ebea6bc3b9ffff0e08b0c4e48f5d
Rushika-15 <rushikachaudhari1572001@gmail.com>
2026-06-16 12:59:57 +0530

    Added some more details

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 # rotate whole array
 
-arr = [1, 2, 3, 4, 5]
-print(arr[-1:0:-1] + arr[:1])
+# arr = [1, 2, 3, 4, 5]
+# print(arr[-1:0:-1] + arr[:1])
 
-# rotate part of array
+# # rotate part of array
+# arr = [1, 2, 3, 4, 5]
+# k = 2
+# print(arr[-k:] + arr[:-k])
+
+# rotate part of array in place
 arr = [1, 2, 3, 4, 5]
 k = 2
-print(arr[-k:] + arr[:-k])
+arr = arr[-k:] + arr[:-k]
+print(arr)
+
